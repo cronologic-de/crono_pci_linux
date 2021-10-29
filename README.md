a08b061a9b9ce75c37a86da2193f705bba7f2d43
Bassem-Ramzy <75851720+Bassem-Ramzy@users.noreply.github.com>
2021-10-29 19:49:36 +0200

    Files from github cronologic_linux_usermode

--- README.md
+++ README.md
@@ -1,2 +1,185 @@
-# crono_pci_linux
+# cronologic_linux_usermode
+Linux user mode driver to support large DMA ring buffers.
 
+It has been developed by cronologic GmbH & Co. KG to support the drivers for its time measurement devices such as.
+* [xHPTDC8](https://www.cronologic.de/products/tdcs/xhptdc8-pcie) 8 channel 13ps streaming time-to-digital converter
+* [xTDC4](https://www.cronologic.de/products/tdcs/xtdc4-pcie) 4 channel 13ps common-start time-to-digital converter
+* [Timetagger](https://www.cronologic.de/products/tdcs/timetagger) 500ps low cost TDCs
+* [Ndigo6G-12](https://www.cronologic.de/products/adcs/ndigo6g-12) 6.4 Gsps 12 bit ADC digitizer board with pulse extraction and 13ps TDC.
+* [Ndigo5G-10](https://www.cronologic.de/products/adcs/cronologic-ndigo5g-10) 5 Gsps 10 bit ADC digitizer board with pulse extraction
+
+However, the module could be useful to any PCIe developer who is using large buffers that are scattered in PCI space but shall look contiguous in user space. 
+
+The kernel mode part of the driver is available in a [separate repository](https://github.com/cronologic-de/cronologic_linux_kernel).
+
+The initial code has been written by [Bassem Ramzy](https://github.com/Bassem-Ramzy) with support from [Richard Weinberger](https://github.com/richardweinberger). It is licensed unter [GPL3](LICENSE).
+
+---
+
+# The Project
+The project generates `crono_userspace.a` static lbrary that is used by applications that need to access the kernel driver.
+
+## Architecture
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/75851720/135757078-e01d9b67-afff-400f-b3e8-d58bd814fed3.png" width="75%" height="75%"/>
+</p>
+
+The userspace in this project represents the `OS Abstraction Layer` in the architecture. It interfaces with the driver module via `ioctl`.
+
+`Crono Kernel Layer` is introduced in the [Driver Kernel Module Repository](https://github.com/cronologic-de/cronologic_linux_kernel).
+
+##  Directory Structure
+    .
+    ├── include        # Header files to be included by application as well
+    ├── userspace-src  # Userspace source files
+    ├── Makefile
+    └── MakefileCommon.mk
+
+## Supported Kernel Versions
+The project is tested on Kernel Versions starting **5.0**.
+
+## Supported Distributions
+The project is tested on the following **64-bit** distributions:
+- **Ubuntu** (ubuntu-20.04.1-desktop-amd64) 
+  - 5.4.0-42-generic
+  - 5.10.0-051000-generic
+  - 5.11.0-37-generic
+- **CentOS** (CentOS-Stream-8-x86_64-20210927)(**)
+  - 5.4.150-1.e18.elrepo.x86_64
+  - 5.14.9-1.el8.elrepo.x86_64
+- **Fedora** (Fedora-Workstation-Live-x86_64-34-1.2)(**)
+  - 5.14.9-200.fc34.x86_64
+- **Debian** (Debian GNU/Linux 11 (bullseye))(**)
+  - 5.10.0-8-amd64
+- **openSUSE** Leap 15.3(**)
+  - 5.3.15-59.27-default
+
+(**) Project code is built successfully, however, it was not tested on the devices.
+
+---
+
+# Build the Project
+To build the project, run `make` command:
+```CMD
+$ make
+```
+
+## Target Output
+The build target output is:
+| Output | Builds | Description | 
+| -------- | ------ | ----------- |
+| `crono_userspace.a` | ``./build/bin/release_64/`` | The release version of the userspace static library |
+| `crono_userspace.a` | ``./build/bin/debug_64/`` | The debug version of the userspace static library |
+| `crono_userspace.so._version_` </br> and </br>`crono_userspace.so` symbolic link | ``./build/crono_userspace/release_64``| The release version of the userspace shared library |
+| `crono_userspace.so._version_` </br> and </br>`crono_userspace.so` symbolic link | ``./build/crono_userspace/debug_64``| The debug version of the userspace shared library |
+
+Temporary build files (e.g. `.o` files) are found under the directory ``./build/crono_userspace``.
+
+## Makefiles and Build Versions
+The following makefiles are used to build the project versions:
+| Makefile | Builds | Description | 
+| -------- | ------ | ----------- |
+| ./Makefile | Debug </br> Release | Calls ALL makefiles in sub-directories. </br>This will build both the `debug` and `release` versions of the project.|
+| ./userspace-src/Makefile | Debug </br> Release | This will build both the `debug` and `release` versions of the project.</br>Make options:</br>- **release_64**: Builds the release version.</br>- **debug_64**: Builds the debug version.</br>- **cleanrelease_64**: Cleans the release version.</br>- **cleandebug_64**: Cleans the debug version.</br>- all: release_64 debug_64.</br>- clean: cleanrelease_64 cleandebug_64.|
+| ./MakefileCommon.mk | None | Contains the common functions used by makefile(s) |
+
+## Build Prerequisites
+### Ubuntu 
+- Make sure that both `make` and `g++` packages are installed, or install them using: 
+```CMD
+sudo apt-get install make g++
+```
+
+### CentOS 
+- Make sure that both `make` and `g++` packages are installed, or install them using: 
+```CMD
+sudo yum install g++ make
+```
+
+### Fedora
+- Make sure that both `make` and `g++` packages are installed, or install them using: 
+```CMD
+sudo yum install g++ make
+```
+- If the kernel development is not installed on your system for the current kernel verision, you can install it as following
+```CMD
+sudo yum install kernel-devel-$(uname -r)
+```
+
+### Debian 
+- Make sure that both `make` and `g++` packages are installed, or install them using: 
+```CMD
+sudo apt-get install make g++
+```
+- Make sure `modules` and `headers` of your current kernel version are installed, or install them using:
+```CMD
+sudo apt-get install linux-headers-$(uname -r) 
+```
+
+### openSUSE
+- Make sure you have **sudo** access.
+- Make sure that both `make` and `g++` packages are installed, or install them using: 
+```CMD
+sudo zypper install make gcc-c++
+```
+- Make sure `modules` and `headers` of your current kernel version are installed, or install them using:
+```CMD
+sudo zypper in kernel-devel kernel-default-devel
+sudo zypper up
+```
+
+### General Notes
+* You can check if `make` and `g++` are installed by running the following commands:
+```CMD
+make -v
+```
+and
+```CMD
+g++ -v
+```
+
+In Fedora, the following will be dipslayed in case `g++` is not installed,
+```CMD
+$ g++ -v
+bash: g++: command not found...
+Install package 'gcc-c++' to provide command 'g++'? [N/y]  
+```
+You just enter `y` and accept installing dependencies, if inquired.
+
+
+## Clean the Output Files 
+To clean the project all builds output:
+```CMD
+make clean
+```
+Or, you can clean a specific build as following:
+```CMD
+.../userspace-src$ sudo make cleandebug_64
+.../userspace-src$ sudo make cleanrelease_64
+```
+
+## Set the Library Version
+Set the library version in the [Makefile](./userspace-src/Makefile):
+```MAKE
+LIBVER		:= .1
+LIBFULLVER	:= $(LIBVER).0.1
+```
+
+## More Details
+
+### Preprocessor Directives
+| Identifier | Description | 
+| ---------- | ----------- |
+|`CRONO_DEBUG_ENABLED` and `DEBUG`| Debug mode.|
+
+---
+
+# Usage 
+The library is provided mainly as a static library `crono_userspace.a` to be used by other applications to handle the devices.
+
+## Header Files
+All the provided APIs and macros are found in the header file [``crono_kernel_interface.h``](./include/crono_kernel_interface.h). 
+
+Additionally, `BAR` and `Configuraion Space` utility functions prototypes are found in [``crono_userspace.h``](./include/crono_userspace.h). 
+
+While, cronologic PCI driver module strucutres and definitions are found in the header file [``crono_driver.h``](./include/crono_driver.h). 
---
+++ include/crono_kernel_interface.h
@@ -0,0 +1,277 @@
+#ifndef __CRONO_KERNEL_INTERFACE_H__
+#define __CRONO_KERNEL_INTERFACE_H__
+
+#ifndef __linux__
+#pragma message("Source code is compliant with Linux only")
+#endif
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include "crono_driver.h"
+#define PCI_ANY_ID (~0)
+#define CRONO_SUCCESS 0
+#define CRONO_VENDOR_ID 0x1A13
+#ifndef FALSE
+#define FALSE (0)
+#endif
+#ifndef TRUE
+#define TRUE (!FALSE)
+#endif
+
+typedef uint64_t pciaddr_t;
+typedef size_t UPTR;
+typedef KPTR PHYS_ADDR;
+
+enum { CRONO_KERNEL_PCI_CARDS = 100 }; // Slots max X Functions max
+
+typedef struct {
+        // Parameters used for string transfers:
+        uint32_t dwBytes;   // For string transfer.
+        uint32_t fAutoinc;  // Transfer from one port/address
+                            // or use incremental range of addresses.
+        uint32_t dwOptions; // Must be 0.
+        union {
+                unsigned char Byte;  // Use for 8 bit transfer.
+                unsigned short Word; // Use for 16 bit transfer.
+                uint32_t Dword;      // Use for 32 bit transfer.
+                uint64_t Qword;      // Use for 64 bit transfer.
+                void *pBuffer;       // Use for string transfer.
+        } Data;
+
+        // Needed for internal projects usage
+        KPTR pPort;        // I/O port for transfer or kernel memory address.
+        uint32_t cmdTrans; // Transfer command WD_TRANSFER_CMD.
+
+} CRONO_KERNEL_TRANSFER;
+
+typedef struct {
+        uint32_t dwDomain;
+        uint32_t dwBus;
+        uint32_t dwSlot;
+        uint32_t dwFunction;
+} CRONO_KERNEL_PCI_SLOT;
+
+typedef struct {
+        uint32_t dwVendorId;
+        uint32_t dwDeviceId;
+} CRONO_KERNEL_PCI_ID;
+
+typedef struct {
+        CRONO_KERNEL_PCI_SLOT pciSlot; /* PCI slot information */
+} CRONO_KERNEL_PCI_CARD_INFO;
+
+/* Handle to device information struct */
+typedef void *CRONO_KERNEL_DEVICE_HANDLE;
+
+/* PCI/PCMCIA slot */
+typedef union {
+} CRONO_KERNEL_SLOT_U;
+
+/* PCI scan results */
+typedef struct {
+        uint32_t dwNumDevices; /* Number of matching devices */
+        CRONO_KERNEL_PCI_ID
+        deviceId[CRONO_KERNEL_PCI_CARDS]; /* Array of matching device IDs */
+        CRONO_KERNEL_PCI_SLOT
+        deviceSlot[CRONO_KERNEL_PCI_CARDS]; /* Array of matching device
+                                             * locations
+                                             */
+} CRONO_KERNEL_PCI_SCAN_RESULT;
+
+typedef uint32_t CRONO_KERNEL_DRV_OPEN_OPTIONS;
+
+/**************************************************************
+  Function Prototypes
+ **************************************************************/
+/* -----------------------------------------------
+    General
+   ----------------------------------------------- */
+/* Get a device's user context */
+void *CRONO_KERNEL_GetDevContext(CRONO_KERNEL_DEVICE_HANDLE hDev);
+
+/* -----------------------------------------------
+    Open/close driver and init/uninit CRONO_KERNEL library
+   ----------------------------------------------- */
+uint32_t CRONO_KERNEL_DriverOpen(CRONO_KERNEL_DRV_OPEN_OPTIONS openOptions);
+uint32_t CRONO_KERNEL_DriverClose(void);
+
+/* -----------------------------------------------
+    Scan bus (PCI/PCMCIA)
+   ----------------------------------------------- */
+uint32_t
+CRONO_KERNEL_PciScanDevices(uint32_t dwVendorId, uint32_t dwDeviceId,
+                            CRONO_KERNEL_PCI_SCAN_RESULT *pPciScanResult);
+
+/* -----------------------------------------------
+    Open/Close device
+   ----------------------------------------------- */
+uint32_t
+CRONO_KERNEL_PciDeviceOpen(CRONO_KERNEL_DEVICE_HANDLE *phDev,
+                           const CRONO_KERNEL_PCI_CARD_INFO *pDeviceInfo,
+                           const void *pDevCtx, void *reserved,
+                           const char *pcKPDriverName, void *pKPOpenData);
+
+uint32_t CRONO_KERNEL_PciDeviceClose(CRONO_KERNEL_DEVICE_HANDLE hDev);
+
+/* -----------------------------------------------
+    Set card cleanup commands
+   ----------------------------------------------- */
+uint32_t CRONO_KERNEL_CardCleanupSetup( //$$ finalize
+    CRONO_KERNEL_DEVICE_HANDLE hDev, CRONO_KERNEL_TRANSFER *Cmd,
+    uint32_t dwCmds, int bForceCleanup);
+
+/* -----------------------------------------------
+    Read/Write memory and I/O addresses
+   ----------------------------------------------- */
+
+/* Read/write a device's address space (8/16/32/64 bits) */
+uint32_t CRONO_KERNEL_ReadAddr8(CRONO_KERNEL_DEVICE_HANDLE hDev,
+                                uint32_t dwAddrSpace, KPTR dwOffset,
+                                unsigned char *val);
+uint32_t CRONO_KERNEL_ReadAddr16(CRONO_KERNEL_DEVICE_HANDLE hDev,
+                                 uint32_t dwAddrSpace, KPTR dwOffset,
+                                 unsigned short *val);
+uint32_t CRONO_KERNEL_ReadAddr32(CRONO_KERNEL_DEVICE_HANDLE hDev,
+                                 uint32_t dwAddrSpace, KPTR dwOffset,
+                                 uint32_t *val);
+uint32_t CRONO_KERNEL_ReadAddr64(CRONO_KERNEL_DEVICE_HANDLE hDev,
+                                 uint32_t dwAddrSpace, KPTR dwOffset,
+                                 uint64_t *val);
+
+uint32_t CRONO_KERNEL_WriteAddr8(CRONO_KERNEL_DEVICE_HANDLE hDev,
+                                 uint32_t dwAddrSpace, KPTR dwOffset,
+                                 unsigned char val);
+uint32_t CRONO_KERNEL_WriteAddr16(CRONO_KERNEL_DEVICE_HANDLE hDev,
+                                  uint32_t dwAddrSpace, KPTR dwOffset,
+                                  unsigned short val);
+uint32_t CRONO_KERNEL_WriteAddr32(CRONO_KERNEL_DEVICE_HANDLE hDev,
+                                  uint32_t dwAddrSpace, KPTR dwOffset,
+                                  uint32_t val);
+uint32_t CRONO_KERNEL_WriteAddr64(CRONO_KERNEL_DEVICE_HANDLE hDev,
+                                  uint32_t dwAddrSpace, KPTR dwOffset,
+                                  uint64_t val);
+
+/* Is address space active */
+int CRONO_KERNEL_AddrSpaceIsActive(CRONO_KERNEL_DEVICE_HANDLE hDev,
+                                   uint32_t dwAddrSpace);
+
+/* -----------------------------------------------
+    Access PCI configuration space
+   ----------------------------------------------- */
+/* Read/write 8/16/32/64 bits from the PCI configuration space.
+   Identify device by handle */
+uint32_t CRONO_KERNEL_PciReadCfg32(CRONO_KERNEL_DEVICE_HANDLE hDev,
+                                   uint32_t dwOffset, uint32_t *val);
+
+uint32_t CRONO_KERNEL_PciWriteCfg32(CRONO_KERNEL_DEVICE_HANDLE hDev,
+                                    uint32_t dwOffset, uint32_t val);
+
+/* -----------------------------------------------
+    DMA (Direct Memory Access)
+   ----------------------------------------------- */
+/* Allocate and lock a contiguous DMA buffer */
+uint32_t CRONO_KERNEL_DMAContigBufLock(CRONO_KERNEL_DEVICE_HANDLE hDev,
+                                       void **ppBuf, uint32_t dwOptions,
+                                       uint32_t dwDMABufSize,
+                                       CRONO_KERNEL_DMA **ppDma);
+
+/**
+ * Lock a Scatter/Gather DMA buffer.
+ * This function calls the user mode process to  create a mapping of virtual
+ * space to a list of chunked physical addresses (mdl). The kernel mode process
+ * also locks the mapping.
+ *
+ * @return DMASGBufLock_parameters, that are now completely filled.
+ * The relevant part (MDL etc.) is stored in the CRONO_KERNEL_DMA struct.
+ * Any error that appear will be stored in the item value.
+ */
+uint32_t CRONO_KERNEL_DMASGBufLock(CRONO_KERNEL_DEVICE_HANDLE hDev, void *pBuf,
+                                   uint32_t dwOptions, uint32_t dwDMABufSize,
+                                   CRONO_KERNEL_DMA **ppDma);
+
+/* Unlock a DMA buffer */
+uint32_t CRONO_KERNEL_DMABufUnlock(CRONO_KERNEL_DEVICE_HANDLE hDev,
+                                   CRONO_KERNEL_DMA *pDma);
+
+/* Address space information struct */
+typedef struct {
+        UPTR
+            pUserDirectMemAddr; /* Memory address for direct user-mode access */
+        size_t dwSize;          // Added for Linux, to be used with munmap
+} CRONO_KERNEL_ADDR_DESC;
+
+/* Device information struct */
+typedef struct CRONO_KERNEL_DEVICE {
+        uint32_t dwVendorId;
+        uint32_t dwDeviceId;
+        CRONO_KERNEL_PCI_SLOT pciSlot;     /* PCI/PCMCIA device slot location
+                                            * information */
+        CRONO_KERNEL_ADDR_DESC *pAddrDesc; /* Array of device's address spaces
+                                            * information */
+        void *pCtx;                        /* User-specific context */
+        CRONO_KERNEL_ADDR_DESC bar_addr; // BAR0 userspace mapped address
+
+        /**
+         * The name of the corresponding `miscdev` file, found under /dev
+         */
+        char miscdev_name[CRONO_MAX_DEV_NAME_SIZE];
+
+        // Needed for internal projects usage
+        uint32_t dwNumAddrSpaces;          /* Total number of device's address
+                                            * spaces */
+
+} CRONO_KERNEL_DEVICE, *PCRONO_KERNEL_DEVICE;
+
+/**
+ * Return Error Code `error_code` if value `value_to_validate` is NULL
+ *
+ * @param value_to_validate[in]: value to be validated, mostly a pointer.
+ * @param error_code[in]: error code to be returned.
+ */
+#define CRONO_RET_ERR_CODE_IF_NULL(value_to_validate, error_code)              \
+        if (NULL == (value_to_validate)) {                                     \
+                return error_code;                                             \
+        }
+/**
+ * Return Error Code `-EINVAL`
+ * if value `value_to_validate` is NULL
+ *
+ * @param value_to_validate[in]: value to be validated, mostly a pointer.
+ */
+#define CRONO_RET_INV_PARAM_IF_NULL(value_to_validate)                         \
+        if (NULL == (value_to_validate)) {                                     \
+                return -EINVAL;                                                \
+        }
+
+/**
+ * Return Error Code `-EINVAL`
+ * if value `value_to_validate` is 0
+ *
+ * @param value_to_validate[in]: value to be validated, mostly a pointer.
+ */
+#define CRONO_RET_INV_PARAM_IF_ZERO(value_to_validate)                         \
+        if (0 == (value_to_validate)) {                                        \
+                return -EINVAL;                                                \
+        }
+
+#define sprintf_s snprintf
+void printFreeMemInfoDebug(const char *msg);
+#ifdef CRONO_DEBUG_ENABLED
+#define CRONO_DEBUG(...) fprintf(stdout, __VA_ARGS__);
+#define CRONO_DEBUG_MEM_MSG(fmt, ...)                                          \
+        fprintf(stdout, "crono Memory Debug Info: " fmt, ...)
+#else // #ifdef CRONO_DEBUG_ENABLED
+#define CRONO_DEBUG(...)
+#define CRONO_DEBUG_MEM_MSG(fmt, ...)
+#endif // #ifdef CRONO_DEBUG_ENABLED
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __CRONO_KERNEL_INTERFACE_H__ */
